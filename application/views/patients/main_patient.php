9c7b56b37d68a8cb0d9465bf7d2cff87635a91e2
kenshin79 <homeruyco@hotmail.com>
2014-05-14 15:44:17 +0800

    change search patient button function

--- application/views/patients/main_patient.php
+++ application/views/patients/main_patient.php
@@ -1,8 +1,9 @@
 
 <div id="search_patients">
 
-<h3>Search (min 3 chars)</h3>
-<input name="search_p" type="text" size="20" id="search_p" onkeyup="searchPatients()"/>
+<h3>Search (faster if more chars)</h3>
+<input name="search_p" type="text" size="20" id="search_p" \/><br />
+<button onclick="searchPatients()">Search!</button>
 
 </div>
 
